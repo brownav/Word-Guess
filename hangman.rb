4b26beb869c82de3923421f418d4ae0527344b30
Alexandria Brown <axbn2190@gmail.com>
2018-02-13 13:15:13 -0800

    made gameplay class, working on guess class

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,55 @@
-class Display
-  attr_accessor :flowers, :secret_word, :guess_letter
 
-  def initialize()
+
+
+
+class GamePlay
+  attr_accessor :play
+
+  def initialize (play)
+    @play = play
+  end
+
+  def quit_or_play_game
+    if @play == "n"
+      puts "Goodbye!"
+      exit
+    elsif @play == "y"
+
+      new_word = GenerateWord.new
+      word_array = new_word.fake_word
+      picture = Display.new(word_array)
+      picture.print_flower
+
+      ## call GenerateWord class
+        # withing GenerateWord call DisplayPicture
+        # call DisplayPicture once when generated, then call updated form in Guess
+
+    end
+  end
+
+end
+
+class GenerateWord
+  attr_accessor :word
+
+  def initialize
+    @word = word
+  end
+
+  def fake_word
+    @word = "desks"
+    @word = @word.split("")
+    return @word
+  end
+
+end
+
+class Display
+  attr_accessor :flowers, :word_array
+
+  def initialize(word_array)
     @flowers = ["(@)", "(@)", "(@)", "(@)"]
+    @word_array = word_array
   end
 
   def print_flower
@@ -19,8 +66,6 @@ class Display
   end
 
   def remove_flower
-    # remove flower from picture
-    # flowers stored as elements in array?
     @flowers.delete_at(-1)
     print_flower
   end
@@ -38,47 +83,40 @@ class Display
 end
 
 
-testdisplay = Display.new
-#testdisplay.print_flower
-testdisplay.display_progress("m")
-testdisplay.remove_flower
+puts "Would you like to play a game of HANGMAN?"
+print "Type Y or N: "
+user_response = gets.chomp.downcase
+
+round1 = GamePlay.new(user_response)
+round1.quit_or_play_game
 
 
-# if !@secret_word.include?(@guess_letter)
-#   remove_flower
+#
+# testdisplay = Display.new
+# #testdisplay.print_flower
+# testdisplay.display_progress("m")
+# testdisplay.remove_flower
+#
+#
+# # if !@secret_word.include?(@guess_letter)
+# #   remove_flower
+# # end
+#   # end game at 0 flowers
+#
+#
+#
+#
+
+#
+#
+# class Guess
+#
+#   def initialize(user_guess)
+#     #
+#   end
+#
+#   def check_guess
+#     # checks user_guess against
+#   end
+#
 # end
-  # end game at 0 flowers
-
-
-
-
-class SecretWord
-  attr_accessor :secret_word
-
-  def initialize
-    @secret_word = secret_word
-  end
-
-  def generate_secret_word
-
-    @secret_word = Faker::
-    # generate random unique word
-    # for testing phase, secret_word = "desks"
-    @secret_word = "desks"
-    return @secret_word
-  end
-
-end
-
-
-class Guess
-
-  def initialize(user_guess)
-    #
-  end
-
-  def check_guess
-    # checks user_guess against
-  end
-
-end
