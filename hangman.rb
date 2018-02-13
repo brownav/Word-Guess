8f105e7209fbc5c9eb06e3a346e90a6a176e8fea
Abiaina <abinnet.a@gmail.com>
2018-02-12 16:10:00 -0800

    prints flower but unable to removed a flower after incorrect guesses

--- hangman.rb
+++ hangman.rb
@@ -1,39 +1,55 @@
 class Display
-  attr_reader :flowers
+  attr_accessor :flowers, :secret_word, :guess_letter
 
   def initialize
     @flowers = %W[(@)(@)(@)(@)]
+    word = "desks"
+    @secret_word = word.split("")
+    @guess_letter = "m"
+
   end
 
   def print_flower
-    @flowers.each do |i|
-      print "#{i}\n"
+    puts "\n"
+    @flowers.each do |flower|
+      print "#{flower}\n"
     end
+    puts " \\,\\,|,/,/,"
+    puts "   _\\|/_   "
+    puts "  |_____|  "
+    puts "   |   |   "
+    puts "   |___|   \n\n"
   end
-
-    #print "#{@flowers}\n"
-
-# print  @flowers
-# puts ",\,\,|,/,/,"
-# puts "   _\|/_   "
-# puts "  |_____|  "
-# puts "   |   |   "
-# puts "   |___|   "
 
   def remove_flower
     # remove flower from picture
     # flowers stored as elements in array?
+    @flowers = [@flowers.shift]
+    print_flower
   end
 
   def display_progress(letter)
-    # displays dashes
     # change dashes to letter for correct guess
+    @guess_letter = letter
+    print "Word: "
+    @secret_word.length.times do
+      print "_"
+    end
+    puts "\n"
+    if !@secret_word.include?(@guess_letter)
+      remove_flower
+      # end game at 0 flowers
+    end
+
   end
 
 end
 
 testdisplay = Display.new
 testdisplay.print_flower
+testdisplay.display_progress("m")
+
+
 
 
 
@@ -52,6 +68,9 @@ class SecretWord
 
   def generate_secret_word
     # generate random unique word
+    # for testing phase, secret_word = "desks"
+    @secret_word = "desks"
+    return @secret_word
   end
 
 end
